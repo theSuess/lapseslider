787bb3a5e123a32543c0d3858c9f576f8c50a787
Dominik Süß <dominik.suess@outlook.at>
2015-09-11 16:08:50 +0200

    Init

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/luismesas/goPi/piface"
+	"github.com/luismesas/goPi/spi"
+	"time"
+)
+
+func main() {
+
+	// creates a new pifacedigital instance
+	pfd := piface.NewPiFaceDigital(spi.DEFAULT_HARDWARE_ADDR, spi.DEFAULT_BUS, spi.DEFAULT_CHIP)
+
+	// initializes pifacedigital board
+	err := pfd.InitBoard()
+	if err != nil {
+		fmt.Printf("Error on init board: %s", err)
+		return
+	}
+
+	// blink time!!
+	fmt.Println("Blinking led 7 each second")
+	for {
+		pfd.Leds[7].Toggle()
+		time.Sleep(time.Second)
+	}
+}
