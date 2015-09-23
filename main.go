9b7e971b60354c0edd0fa06643bed03a9a5d82ec
Dominik Süß <dominik.suess@outlook.at>
2015-09-23 17:03:31 +0200

    motor code

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"fmt"
 	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
 	"github.com/luismesas/goPi/piface"
 	"github.com/luismesas/goPi/spi"
 	"github.com/unrolled/render"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 var progress int
+var running bool
 
 func main() {
 	// Render engine
@@ -19,22 +22,47 @@ func main() {
 	})
 
 	// Handlers
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		r.HTML(w, http.StatusOK, "home", progress)
 	})
-	mux.HandleFunc("/about", func(w http.ResponseWriter, req *http.Request) {
+	router.HandleFunc("/about", func(w http.ResponseWriter, req *http.Request) {
 		r.HTML(w, http.StatusOK, "about", nil)
 	})
-	mux.HandleFunc("/blink", func(w http.ResponseWriter, req *http.Request) {
-		go testMotor()
-		r.Text(w, http.StatusOK, "OK")
-	})
+	router.HandleFunc("/start", func(w http.ResponseWriter, req *http.Request) {
+		time := req.FormValue("time")
+		stepsPerMinute, _ := strconv.Atoi(req.FormValue("steps"))
+		var hours, minutes, seconds int
+		fmt.Sscanf(time, "%d:%d:%d", &hours, &minutes, &seconds)
+		go startLapse((hours*3600)+(minutes*60)+seconds, stepsPerMinute)
+		r.HTML(w, http.StatusOK, "home", progress)
+	}).Methods("POST")
 
 	// HTTP Server
 	n := negroni.Classic()
-	n.UseHandler(mux)
+	n.UseHandler(router)
 	n.Run(":3000")
+}
+
+func startLapse(seconds int, stepsPerMinute int) {
+	running = true
+	seconds = seconds * 10
+	var pauseTime float64
+	pauseTime = float64(stepsPerMinute) / float64(60.0)
+	pfd := piface.NewPiFaceDigital(spi.DEFAULT_HARDWARE_ADDR, spi.DEFAULT_BUS, spi.DEFAULT_CHIP)
+
+	// initializes pifacedigital board
+	err := pfd.InitBoard()
+	if err != nil {
+		fmt.Printf("Error on init board: %s", err)
+		return
+	}
+
+	for i := 0; i < seconds && running; i++ {
+		pfd.OutputPins[0].Toggle()
+		time.Sleep(time.Duration(float64(time.Second) * pauseTime))
+		pfd.OutputPins[0].Toggle()
+	}
 }
 
 func testMotor() {
@@ -48,6 +76,6 @@ func testMotor() {
 		return
 	}
 	pfd.OutputPins[0].Toggle()
-	time.Sleep(time.Second * 5)
+	time.Sleep(time.Second / 10)
 	pfd.OutputPins[0].Toggle()
 }
